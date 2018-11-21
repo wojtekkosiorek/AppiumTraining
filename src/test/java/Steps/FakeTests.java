dd5551963ae2d212d54003722095ddb2869ffc58
Wojtek Kosiorek <ext-wojciech.kosiorek@finago.com>
2018-11-21 10:02:46 +0100

    as

--- src/test/java/Steps/FakeTests.java
+++ src/test/java/Steps/FakeTests.java
@@ -1,15 +1,24 @@
 package Steps;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
 
+@Epic("All epic test")
+@Feature("Feature")
 public class FakeTests {
 
     @Test (groups = {"smoke","regression"})
+    @Story("Fake smoke test")
+    @Description("Empty test description")
     public void SmokeTest() {
     }
 
 
     @Test (groups = {"regression"})
+    @Story("Fake rege test")
     public void RegressionTest() {
     }
 }
